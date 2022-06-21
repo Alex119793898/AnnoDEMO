fa44ed7a9fbe328c33004946508651d097cb2bc8
csl119793898 <119793898@qq.com>
2022-06-21 22:50:56 +0800

    1.增加工具类 DBUtil 执行生成的 update / delete 语句

--- src/test/java/com/caoliang/ReflectTest.java
+++ src/test/java/com/caoliang/ReflectTest.java
@@ -1,22 +1,30 @@
 package com.caoliang;
 
 import com.caoliang.entity.OrderInfo;
+import com.caoliang.util.DBUtil;
 import com.caoliang.util.SQLProvider;
+
+import java.sql.Connection;
+import java.sql.Statement;
 
 public class ReflectTest {
     public static void main(String[] args) throws Exception {
 
         OrderInfo orderInfo = new OrderInfo();
         orderInfo.setOrderId("10001");
-        orderInfo.setClientName("客户1");
+        orderInfo.setClientName("客户3");
 
         SQLProvider sqlProvider = new SQLProvider();
 
         //String sql = sqlProvider.genDeleteSql(orderInfo);
-        //String sql = sqlProvider.genUpdateSql(orderInfo);
-        String sql = sqlProvider.genQuerySql(orderInfo);
+        String sql = sqlProvider.genUpdateSql(orderInfo);
+        //String sql = sqlProvider.genQuerySql(orderInfo);
 
+        Connection connection = DBUtil.getConnection();
+        Statement statement = connection.createStatement();
+        int i = statement.executeUpdate(sql);
 
         System.out.println(sql);
+        System.out.println(i);
     }
 }
